d151edb855de0d0284d093fd20e7be2a20d04c52
CJ Sibanda <87155971+cjsibanda@users.noreply.github.com>
2026-09-20 13:51:49 -0400

    started App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,59 @@
+////////////////////////////////////////
+//The Root Application Assembly
+//Pseudo...
+///////////////////////////////////////////
+import React, {useState} from 'react';
+import { Account } from './types/account';
+// sidebar
+// coponents
+
+const MOCK_ACCOUNTS: Account[] = [
+  {
+    id: 'sibanda_1',
+    accountNumberMasked: '...1234',
+    nickname: 'Primary Checking',
+    type: 'CHECKING',
+    balanceCents: 100500,
+    currency: 'CAD',
+    availableBalanceCents: 500100,
+  },
+  {
+    id: 'sibanda_2',
+    accountNumberMasked: '...9876',
+    nickname: 'Platnum Credit Card',
+    type: 'CREDIT_CARD',
+    balanceCents: -10050,
+    currency: 'CAD',
+  },
+];
+
+export const App: react.FC = () => {
+  const [activeTab, setActiveTab] = useState('accounts');
+
+  return (
+    <div>
+      {/* The Header*/}
+      <header>
+        //The header
+      </header>
+
+      {/* Main Container*/}
+      <div>
+        <Sidebar activeTab={activeTab}/>
+
+        {/* Content Area */}
+        <main>
+          //header? 
+          // main section
+        </main>
+        
+        {/* Right Info Panel */}
+        <aside>
+          //div?
+        </aside>
+      </div>
+    </div>
+  );
+};
+
+export default App;
